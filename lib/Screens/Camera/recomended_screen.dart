f55980f08d8d37fc8b5e945b526cd262fdf17fa1
Khalifa <16abdulrhmanahmed@gmail.com>
2024-06-05 19:49:11 +0200

    add le to price

--- lib/Screens/Camera/recomended_screen.dart
+++ lib/Screens/Camera/recomended_screen.dart
@@ -133,7 +133,7 @@ class _RecomendedProductsScreenState extends State<RecomendedProductsScreen> {
                                 Align(
                                   alignment: Alignment.bottomLeft,
                                   child:
-                                  Text(product['price'].toString()),
+                                  Text('${product['price'].toString()} l.e'),
                                 ),
                               ],
                             ),
