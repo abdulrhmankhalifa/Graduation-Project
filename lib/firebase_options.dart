e1008dbb0ae81581e98fb9d68347892da3a5258e
moazelsawaf <moazelsawaf@hotmail.com>
2024-06-05 04:49:46 +0300

    fix: :wrench: fix firebase integration

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -17,7 +17,10 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      return web;
+      throw UnsupportedError(
+        'DefaultFirebaseOptions have not been configured for web - '
+        'you can reconfigure this by running the FlutterFire CLI again.',
+      );
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
@@ -25,7 +28,10 @@ class DefaultFirebaseOptions {
       case TargetPlatform.iOS:
         return ios;
       case TargetPlatform.macOS:
-        return macos;
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for macos - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
       case TargetPlatform.windows:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for windows - '
@@ -43,18 +49,9 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyC6t3bD00gCc9T4EXTI_KM9CrnwxHCt5n4',
-    appId: '1:931943906136:web:1d9d4f1c8dcfc04dbbc32d',
-    messagingSenderId: '931943906136',
-    projectId: 'graduationproject-52a21',
-    authDomain: 'graduationproject-52a21.firebaseapp.com',
-    storageBucket: 'graduationproject-52a21.appspot.com',
-  );
-
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyA79fJNf76jYHha7p7SXxgzytin9kEC-ps',
-    appId: '1:931943906136:android:f4afe49913bc1ffebbc32d',
+    appId: '1:931943906136:android:e0d876b15cc4cbd1bbc32d',
     messagingSenderId: '931943906136',
     projectId: 'graduationproject-52a21',
     storageBucket: 'graduationproject-52a21.appspot.com',
@@ -62,19 +59,10 @@ class DefaultFirebaseOptions {
 
   static const FirebaseOptions ios = FirebaseOptions(
     apiKey: 'AIzaSyA5_81pr5RI9eDUpmwQ2UTD0EFLqUVRgx8',
-    appId: '1:931943906136:ios:aa88c1d38c374522bbc32d',
+    appId: '1:931943906136:ios:f3e73aab7c76e410bbc32d',
     messagingSenderId: '931943906136',
     projectId: 'graduationproject-52a21',
     storageBucket: 'graduationproject-52a21.appspot.com',
-    iosBundleId: 'com.example.graduationProject',
-  );
-
-  static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyA5_81pr5RI9eDUpmwQ2UTD0EFLqUVRgx8',
-    appId: '1:931943906136:ios:7385e561b2a2ff8fbbc32d',
-    messagingSenderId: '931943906136',
-    projectId: 'graduationproject-52a21',
-    storageBucket: 'graduationproject-52a21.appspot.com',
-    iosBundleId: 'com.example.graduationProject.RunnerTests',
+    iosBundleId: 'com.beautymate.app',
   );
 }
